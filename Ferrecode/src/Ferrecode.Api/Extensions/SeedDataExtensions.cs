15a234cdb0aef9dc1cf837bd94220059f4f9e33a
juankmanca <juankmanca@gmail.com>
2024-03-13 18:08:09 -0500

    Seed Database

---
+++ Ferrecode/src/Ferrecode.Api/Extensions/SeedDataExtensions.cs
@@ -0,0 +1,68 @@
+﻿using Bogus;
+using Dapper;
+using Ferrecode.Application.Abstractions.Data;
+using Ferrecode.Domain.Productos;
+using System.Globalization;
+
+namespace Ferrecode.Api.Extensions
+{
+    public static class SeedDataExtensions
+    {
+        public static void SeedData(this IApplicationBuilder app)
+        {
+            using var scope = app.ApplicationServices.CreateScope();
+
+            //Conexion con la cual esta trabajando Dapper
+            var sqlConnectionFactory = scope.ServiceProvider.GetRequiredService<ISqlConnectionFactory>();
+
+            using var connection = sqlConnectionFactory.CreateConnection();
+
+            var faker = new Faker();
+
+            List<object> productos = new();
+
+            for(var i = 0; i < 50;i++)
+            {
+                productos.Add(new
+                {
+                    ID = Guid.NewGuid(),
+                    Nombre = faker.Commerce.ProductName(),
+                    FechaCreacion = DateTime.UtcNow,
+                    FechaActualizacion = DateTime.UtcNow,
+                    Precio = decimal.Parse(faker.Commerce.Price(1, 500000), CultureInfo.InvariantCulture),
+                    ValorMedida = decimal.Parse(faker.Commerce.Price(1, 50), CultureInfo.InvariantCulture),
+                    Peso = decimal.Parse(faker.Commerce.Price(1, 50), CultureInfo.InvariantCulture),
+                    VolumenEmpaque = decimal.Parse(faker.Commerce.Price(1, 100), CultureInfo.InvariantCulture),
+                    UnidadDeMedida = "mt"
+                });
+            }
+
+            const string sql = """
+                INSERT INTO [dbo].[Productos]
+                      ([ID]
+                      ,[Precio]
+                      ,[ValorMedida]
+                      ,[UnidadDeMedida]
+                      ,[Peso]
+                      ,[VolumenEmpaque]
+                      ,[Nombre]
+                      ,[FechaCreacion]
+                      ,[FechaActualizacion])
+                VALUES
+                      (
+                        @ID,
+                        @Precio,
+                        @ValorMedida,
+                        @UnidadDeMedida,
+                        @Peso,
+                        @VolumenEmpaque,
+                        @Nombre,
+                        @FechaCreacion,
+                        @FechaActualizacion
+                      ) 
+                """;
+
+            connection.Execute(sql, productos);
+        }
+    }
+}
--- Ferrecode/src/Ferrecode.Api/Program.cs
+++ Ferrecode/src/Ferrecode.Api/Program.cs
@@ -27,7 +27,7 @@ if (app.Environment.IsDevelopment())
 //app.UseHttpsRedirection();
 
 app.ApplyMigration();
-//app.SeedData();
+app.SeedData();
 
 //app.UseCustomExceptionHandler();
 
